56a65e9c7cca7a04418c88912156df523d5262f6
ogDevOps <109356215+CLHdevOps@users.noreply.github.com>
2025-12-22 15:35:17 -0600

    hero edit

--- src/components/hero.js
+++ src/components/hero.js
@@ -20,13 +20,13 @@ const Hero = ({ title, description }) => {
       <section className="h-100 d-flex align-items-center text-center bg-dark-40">
         <Container>
           <main className="hero-content">
-            <h1 className="title">{title}</h1>
-            <hr className="h-separator" />
-            <p className="hero-description">{description}</p>
             <section className="hero-buttons">
               <Button href="#services">Our Services</Button>
               <Button href="#quote" outline>Request a Quote</Button>
             </section>
+            <h1 className="title">{title}</h1>
+            <hr className="h-separator" />
+            <p className="hero-description">{description}</p>
           </main>
         </Container>
       </section>
