f249fbe43bc737d61eb503c4750a6bbe3822343c
ogDevOps <corey@verticalconsulting.net>
2026-01-09 16:08:38 -0600

    removed images

--- src/html.js
+++ src/html.js
@@ -12,31 +12,46 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M3W35JTV');`
-        }} />
+})(window,document,'script','dataLayer','GTM-M3W35JTV');`,
+          }}
+        />
         {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17864041271"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17864041271"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'AW-17864041271');`
-        }} />
+  gtag('config', 'AW-17864041271');`,
+          }}
+        />
+        {/* Klarna On-Site Messaging SDK */}
+        <script
+          async
+          src="https://js.klarna.com/web-sdk/v1/klarna.js"
+          data-client-id="on-site-messaging"
+        ></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
         {/* Google Tag Manager (noscript) */}
-        <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3W35JTV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-        }} />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3W35JTV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         {/* End Google Tag Manager (noscript) */}
         {props.preBodyComponents}
         <div
